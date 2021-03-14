2445965ea88b606f5e31ddb5f063746a5469e835
healthyxj <62688551+healthyxj@users.noreply.github.com>
2021-03-14 09:40:51 +0800

    Create 206反转链表.c

---
+++ List/LeetCode/206反转链表.c
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+/*
+题目要求反转链表，通常的解决方法是定义pre、cur、nex三个指针，然后改变指向完成反转
+*/
+
+struct ListNode* reverseList(struct ListNode* head){
+    struct ListNode* pre; //最好分开来定义指针
+    struct ListNode* p = head;  //定义一个临时指针指向头指针
+    struct ListNode* nex;
+    pre = NULL; //先将前指针指向空
+
+    while(p){
+        nex = p->next; //定义一个nex指针，用于遍历整个链表
+        p->next = pre;
+        pre = p;
+        p = nex;  //用于最后空结点的判断
+    }
+    
+    return pre;
+}
